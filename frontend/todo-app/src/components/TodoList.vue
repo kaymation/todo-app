9fcf85c0f525b3f455978c17dc8e887b88c6bf10
Kevin Quigley <kevinmquigley.41@gmail.com>
2023-06-14 22:06:13 -0700

    add/edit functionality

--- frontend/todo-app/src/components/TodoList.vue
+++ frontend/todo-app/src/components/TodoList.vue
@@ -23,27 +23,43 @@
                             </v-col>
                             <v-col md="2">
                                 <div class="new-button-holder">
-                                    <v-btn color="green" @click="addItem">Add Item</v-btn>
+                                    <v-btn v-if="!newItemOpen"
+                                        color="green"
+                                        @click="newItemOpen = true">Add Item</v-btn>
                                 </div>
                             </v-col>
                         </v-row>
+
+                            <v-card v-if="newItemOpen" class="mt-4 pa-4">
+                                <v-textarea v-model="newItemText" placeholder="Enter your task!"></v-textarea>
+
+                                <v-divider></v-divider>
+                                <v-card-actions>
+                                    <v-btn color="green" outline @click="addItem">Save</v-btn>
+                                    <v-btn color="red" outline @click="closeNew">Cancel</v-btn>
+                                </v-card-actions>
+                            </v-card>
+ 
                     </v-col>
                 </v-row>
                 <v-row justify="center">
                     <v-col cols="md-8 sm-10">
                         <v-card v-for="item in items" :key="item.key" class="ma-2" :color="(item.done) ? 'green' : 'primary'">
                             <template v-if="editingId == item.id">
-                                <v-row class="pa-4">
-                                    <v-textarea :value="item.body"></v-textarea>
-                                </v-row>
-                                <v-row>
-                                    <v-btn @click="saveItem(item)">Save</v-btn>
-                                </v-row>
+                                    <v-card class="mt-4 pa-4">
+                                        <v-textarea v-model="editItemBody"></v-textarea>
+                                        <v-divider></v-divider>
+                                        <v-card-actions>
+                                            <v-btn @click="editItem(item)">Save</v-btn>
+                                            <v-btn color="red" outline @click="closeEdit">Cancel</v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+
                             </template>
                             <template v-else>
                                 <v-row class="pa-4">
                                     <v-col cols="md-8" sm="5">
-                                        <span>{{ item.body }} {{ item.id}}</span>
+                                        <span>{{ item.body }}</span>
                                     </v-col>
                                     <v-col md="3" sm="2" offset="md-1" offset-sm="0">
                                         <div class="actions-holder">
@@ -110,7 +126,7 @@
 
 <script>
 
-import { getItems, updateItem, destroyItem } from '../fetch/items'
+import { getItems, updateItem, destroyItem, createItem } from '../fetch/items'
 import { arrayMove } from '../etc/utils'
 
 export default {
@@ -124,6 +140,9 @@ export default {
             items: [],
             editingId: null,
             loading: true,
+            newItemOpen: false,
+            newItemText: "",
+            editItemBody: "",
             deleteDialog: false,
         }
     },
@@ -150,13 +169,22 @@ export default {
             } else {
                 newIndex = this.items.length - 1
             }
-            console.log(newIndex)
             arrayMove(this.items, toToggleIndex, newIndex)
             this.items[newIndex].done = newStatus
             this.loading = false
 
         },
-        async addItem(item) {
+        async addItem() {
+            this.loading = true
+            const newItem = {
+                done: false,
+                body: this.newItemText,
+            }
+
+            const item = await createItem(newItem)
+            this.newItemOpen = false
+            this.items.splice(0, 0, item)
+            this.loading = false
         
         },
         async deleteItem(itemId) {
@@ -167,11 +195,40 @@ export default {
             this.items.splice(toDeleteIndex, 1)
             this.loading = false
         },
+        async editItem(item) {
+            this.loading = true
+            const changes = {
+                done: item.done,
+                body: this.editItemBody,
+            }
+            await updateItem(changes, item.id)
+
+            const savedItem = {
+                id: item.id,
+                body: this.editItemBody,
+                done: item.done
+
+            }
+            const itemIndex = this.findIndexById(item.id)
+            this.items[itemIndex] = savedItem
+            this.editingId = null
+            this.loading = false
+
+        },
         findIndexById(id) {
             return this.items.findIndex(item => item.id === id)
+        },
+        openEdit(item) {
+            this.editItemBody = item.body
+            this.editingId = item.id
+        },
+        closeEdit() {
+            this.editingId = null
+        },
+        closeNew() {
+            this.newItemOpen = false
         }
     }
-
 }
 </script>
 
